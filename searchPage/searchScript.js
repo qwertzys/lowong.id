55d9bc370174ab64098cb57dc7d319a1a32f38dd
qwertzys <greg3ananda@gmail.com>
2024-06-17 21:38:06 +0700

    Search needs to be in progress (i might be cooked fellas)

---
+++ searchPage/searchScript.js
@@ -0,0 +1,13 @@
+const sidebar = document.querySelector('.sidebar');
+
+c
+
+// Navbar Functions
+
+function showSideBar(){
+    sidebar.classList.add('open');
+}
+
+function hideSideBar(){
+    sidebar.classList.remove('open');
+}
